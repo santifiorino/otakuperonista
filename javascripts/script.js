b8d5a3dcee3d493c0524dca405d78cc94e1a70bc
santifiorino <fiorinosanti@gmail.com>
2021-11-27 17:35:46 -0300

    codigo comentado y mejorado

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,45 +1,53 @@
+// Modal de cuando perdes
 var myModal = new bootstrap.Modal(document.getElementById('myModal'), {
   keyboard: false
-});
+})
+
 var botonOutaku = document.getElementById("botonOtaku");
 var botonPeronista = document.getElementById("botonPeronista");
-
+// En foto1 pongo las previews, en foto2 pongo las fotos enteras
+var foto1 = document.getElementById("foto1");
+var foto2 = document.getElementById("foto2");
+// listaTodos tiene tuplas con un 0 o un 1 en la primer posicion
+// 0: otaku, 1: peronista, y el id de la foto en la segunda posicion
 var listaTodos = [];
 var puntaje = 0;
 var maxPuntaje = 0;
 init();
 
 function init(){
+  // Cantidad de fotos en la base de datos
   var cantOtakus = 60;
   var cantPeronistas = 60;
-
+  // Inicializo listaTodos
   var listaOtakus = [];
   var listaPeronistas = [];
-
   for(var i = 0; i < cantOtakus; i++){
     var id = getID(i);
     listaOtakus.push([0, id]);
   }
-
   for(var i = 0; i < cantPeronistas; i++){
     var id = getID(i);
     listaPeronistas.push([1, id]);
   }
   listaTodos = listaOtakus.concat(listaPeronistas);
   randomizar();
-  var img = document.getElementById("foto");
-  img.src = getLink(listaTodos[0], 1);
+  // Cargo la primer foto y la muestro
+  siguienteFoto();
 }
-
+// Mezcla la listaTodos
 function randomizar(){
   listaTodos = listaTodos.sort((a, b) => 0.5 - Math.random());
 }
-
+// Funcion que llama cuando tocas el boton Otaku o Peronista
 function guessed(guess){
-  var img = document.getElementById("foto");
-  img.src = getLink(listaTodos[puntaje], 0);
+  // Muestro la foto entera
+  foto1.style.display = "none";
+  foto2.style.display = "block";
+  // Inhabilito los botones para evitar que lo toquen antes de que cargue la siguiente imagen
   botonOtaku.setAttribute('onclick','');
   botonPeronista.setAttribute('onclick','');
+  // Coloreo los botones de verde y rojo dependiendo si acertó o erró
   if (listaTodos[puntaje][0] == 0) {
     botonOtaku.className = "btn boton btn-lg botonAcierto";
     botonPeronista.className = "btn boton btn-lg botonError";
@@ -47,13 +55,17 @@ function guessed(guess){
     botonOtaku.className = "btn boton btn-lg botonError";
     botonPeronista.className = "btn boton btn-lg botonAcierto";
   }
+  // Chequeo si adivinaste
   if (guess == listaTodos[puntaje][0]){
+    // Sumo 1 al puntaje, chequeo maxScore y actualizo la interfaz
     puntaje++;
     if (puntaje > maxPuntaje){
       maxPuntaje = puntaje;
     }
     actualizarScores();
+    // Si el puntaje es igual a la cantidad total de fotos, ganaste
     if (puntaje == listaTodos.length) {
+      // Escribo en el modal que ganaste y lo muestro
       var p = document.getElementById("textoPerdiste");
       textoPerdiste.innerHTML = "SCORE: " + puntaje;
       var perdiste = document.getElementById("ganasteperdiste");
@@ -63,11 +75,12 @@ function guessed(guess){
       setTimeout("siguienteFoto()", 1750);
     }
   } else {
+    // Escribo en el modal que perdiste y lo muestro
     var p = document.getElementById("textoPerdiste");
     textoPerdiste.innerHTML = "SCORE: " + puntaje;
     var perdiste = document.getElementById("ganasteperdiste");
     perdiste.innerHTML = "PERDISTE";
-    setTimeout("myModal.toggle()", 1750);
+    setTimeout("myModal.toggle()", 750);
   }
 }
 
@@ -77,7 +90,7 @@ function actualizarScores(){
   var p2 = document.getElementById("maxScore");
   p2.innerHTML = "MAX SCORE: " + maxPuntaje;
 }
-
+// Reiniciar: randomiza la lista de nuevo, pone el puntaje en 0 y arranca
 function reiniciar(){
   randomizar();
   puntaje = 0;
@@ -87,10 +100,12 @@ function reiniciar(){
 }
 
 function siguienteFoto(){
-  var img = document.getElementById("foto");
-  img.src = getLink(listaTodos[puntaje], 1);
-
-
+  // cargo las fotos y muestro el preview
+  foto1.src = getLink(listaTodos[puntaje], 1);
+  foto2.src = getLink(listaTodos[puntaje], 0);
+  foto1.style.display = "block";
+  foto2.style.display = "none";
+  // Pongo los botones en blanco de nuevo y los habilito
   botonOtaku.className = "btn boton btn-lg";
   botonPeronista.className = "btn boton btn-lg";
   botonOtaku.setAttribute('onclick','guessed(0)');
