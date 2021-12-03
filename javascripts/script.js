aac2498f0d4804cb60ca99ed743473810af03156
santifiorino <fiorinosanti@gmail.com>
2021-12-02 21:11:34 -0300

    codigo mucho mas limpio usando clases

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,52 +1,59 @@
-// Modal de cuando perdes
+class Imagen {
+  constructor(id, tipo, link) {
+    this.id = id; // Id de la foto
+    this.tipo = tipo; // Otaku o Peronista
+  }
+}
+// Variables globales
 var myModal = new bootstrap.Modal(document.getElementById('myModal'), {
   keyboard: false
 })
-// Los botones de adivinar
-var botonOutaku = document.getElementById("botonOtaku");
-var botonPeronista = document.getElementById("botonPeronista");
-// La foto del juego
-var foto = document.getElementById("foto");
-// listaTodos tiene tuplas con un 0 o un 1 en la primer posicion
-// 0: otaku, 1: peronista, y el id de la foto en la segunda posicion
-var listaTodos = [];
+var listaImagenes = [];
 var puntaje = 0;
 var maxPuntaje = 0;
 init();
 
 function init(){
-  // Cantidad de fotos en la base de datos
-  const cantOtakus = 60;
-  const cantPeronistas = 60;
-  // Inicializo listaTodos
-  let listaOtakus = [];
-  let listaPeronistas = [];
-  for(let i = 0; i < cantOtakus; i++){
-    let id = getID(i);
-    listaOtakus.push([0, id]);
+  // Inicializo listaImagenes
+  const fotosPorCategoria = 60;
+  for(let i = 0; i < fotosPorCategoria; i++){
+    listaImagenes.push(new Imagen(getID(i), "otaku"));
+    listaImagenes.push(new Imagen(getID(i), "peronista"));
   }
-  for(let i = 0; i < cantPeronistas; i++){
-    let id = getID(i);
-    listaPeronistas.push([1, id]);
-  }
-  listaTodos = listaOtakus.concat(listaPeronistas);
   randomizar();
-  // Cargo la primer foto y la muestro
+  // Cargo la primer foto
   siguienteFoto();
 }
-// Mezcla la listaTodos
+
+// Mezcla la listaImagenes
 function randomizar(){
-  listaTodos = listaTodos.sort((a, b) => 0.5 - Math.random());
+  listaImagenes = listaImagenes.sort((a, b) => 0.5 - Math.random());
 }
-// Funcion que llama cuando tocas el boton Otaku o Peronista
+
+function siguienteFoto(){
+  // Cargo la foto de la mano sola
+  let foto = document.getElementById("foto");
+  let botonOutaku = document.getElementById("botonOtaku");
+  let botonPeronista = document.getElementById("botonPeronista");
+  foto.src = getLink(listaImagenes[puntaje], 1);
+  // Pongo los botones en blanco y los habilito
+  botonOtaku.className = "btn boton btn-lg";
+  botonPeronista.className = "btn boton btn-lg";
+  botonOtaku.setAttribute('onclick','guessed("otaku")');
+  botonPeronista.setAttribute('onclick','guessed("peronista")');
+}
+
+// Funcion que llama cuando tocas el boton de adivinar
 function guessed(guess){
   // Muestro la foto entera
-  foto.src = getLink(listaTodos[puntaje], 0);
-  // Inhabilito los botones para evitar que lo toquen antes de que cargue la siguiente imagen
+  let botonOutaku = document.getElementById("botonOtaku");
+  let botonPeronista = document.getElementById("botonPeronista");
+  let foto = document.getElementById("foto");
+  foto.src = getLink(listaImagenes[puntaje], 0);
+  // Coloreo los botones y los inhabilito hasta mostrar la siguiente foto
   botonOtaku.setAttribute('onclick','');
   botonPeronista.setAttribute('onclick','');
-  // Coloreo los botones de verde y rojo dependiendo si acertó o erró
-  if (listaTodos[puntaje][0] == 0) {
+  if (listaImagenes[puntaje].tipo == "otaku") {
     botonOtaku.className = "btn boton btn-lg botonAcierto";
     botonPeronista.className = "btn boton btn-lg botonError";
   } else {
@@ -54,7 +61,7 @@ function guessed(guess){
     botonPeronista.className = "btn boton btn-lg botonAcierto";
   }
   // Chequeo si adivinaste
-  if (guess == listaTodos[puntaje][0]){
+  if (guess == listaImagenes[puntaje].tipo){
     // Sumo 1 al puntaje, chequeo maxScore y actualizo la interfaz
     puntaje++;
     if (puntaje > maxPuntaje){
@@ -62,11 +69,11 @@ function guessed(guess){
     }
     actualizarScores();
     // Si el puntaje es igual a la cantidad total de fotos, ganaste
-    if (puntaje == listaTodos.length) {
+    if (puntaje == listaImagenes.length) {
       // Escribo en el modal que ganaste y lo muestro
-      var p = document.getElementById("textoPerdiste");
+      let p = document.getElementById("textoPerdiste");
       textoPerdiste.innerHTML = "SCORE: " + puntaje;
-      var perdiste = document.getElementById("ganasteperdiste");
+      let perdiste = document.getElementById("ganasteperdiste");
       perdiste.innerHTML = "GANASTE";
       myModal.toggle();
     } else {
@@ -96,33 +103,23 @@ function reiniciar(){
   myModal.toggle();
   siguienteFoto();
 }
-
-function siguienteFoto(){
-  // cargo las fotos y muestro el preview
-  foto.src = getLink(listaTodos[puntaje], 1);
-  // Pongo los botones en blanco de nuevo y los habilito
-  botonOtaku.className = "btn boton btn-lg";
-  botonPeronista.className = "btn boton btn-lg";
-  botonOtaku.setAttribute('onclick','guessed(0)');
-  botonPeronista.setAttribute('onclick','guessed(1)');
-}
-
-function getLink(tupla, prev){
-  if (tupla[0] == 0){
+// Le das el id y si prev esta en 0 te da la foto de la mano sola, y si está en 1 la foto completa
+function getLink(img, prev){
+  if (img.tipo == "otaku"){
     if (prev == 1){
-      return otaku_links[tupla[1]][0];
+      return otaku_links[img.id][0];
     } else {
-      return otaku_links[tupla[1]][1];
+      return otaku_links[img.id][1];
     }
   } else {
     if (prev == 1){
-      return peronista_links[tupla[1]][0];
+      return peronista_links[img.id][0];
     } else {
-      return peronista_links[tupla[1]][1];
+      return peronista_links[img.id][1];
     }
   }
 }
-
+// Formato de ids
 function getID(i){
   let id = "";
   if (i < 10) {
